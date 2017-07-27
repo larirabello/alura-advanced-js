43d54433e0f7b4e783388290f9920827102d4a9c
GFT\last <last@gft.com>
2017-07-27 14:25:56 -0300

    Including js file

--- client/js/index.js
+++ client/js/index.js
@@ -5,3 +5,28 @@ var campos = [
 ];
 
 console.log(campos);
+
+
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+    var tr = document.createElement('tr');
+
+    campos.forEach(function(campo) {
+        var td = document.createElement('td');
+        //coloca conteudo texto dentro da tag td
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+
+    tdVolume.textContent = campos[1].value * campos[2].value;
+
+    tr.appendChild(tdVolume);
+    tbody.appendChild(tr);
+
+});
