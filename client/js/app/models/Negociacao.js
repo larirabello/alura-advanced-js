b9e5382dfe78979ffb402793fd409e2d062c2e9a
GFT\last <last@gft.com>
2017-07-27 14:51:39 -0300

    Creating class Negociacao

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,9 @@
+class Negociacao {
+
+    //definir os atributos da classe em ES6
+    constructor() {
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+}
