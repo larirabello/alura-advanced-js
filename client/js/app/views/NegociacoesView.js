fb212b0908a8f10f09f81853865690f687ac879d
Larissa Rabello <lari.rabello@gmail.com>
2017-08-15 19:48:05 -0300

    adicionando herança

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,8 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
     constructor(elemento) {
-
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -38,8 +37,4 @@ class NegociacoesView {
             `;
     }
 
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
-    }
 }
