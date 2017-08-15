1111411b0725606b8b4a263f848fb1964dcf85a5
GFT\last <last@gft.com>
2017-08-15 14:18:11 -0300

    Novo helper e controller

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,27 @@
+class DateHelper {
+
+    constructor() {
+
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static dataParaTexto(data) {
+
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`    
+    
+            data.getDate() 
+            + '/' + (data.getMonth() + 1)
+            + '/' + data.getFullYear();
+
+    }
+
+    static textoParaData(texto) {
+
+        //aqui ele verifica se está no padrão e lança erro se não estiver
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) {
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+        } 
+
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
+}
---
+++ client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,21 @@
+// criar modelo de lista de negociacoes para encapsular a lista no model
+class ListaNegociacoes {
+
+    constructor() {
+
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao) {
+        
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+        
+        // para blindar o array, criei no retorno um array vazio e concatenei com o existente. Consigo dar push mas só na cópia e nao na lista original
+        return [].concat(this._negociacoes);
+
+    }
+
+}
