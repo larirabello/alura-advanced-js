305eec77c37370c34b1aba901e4759adec3a87d3
GFT\last <last@gft.com>
2017-08-17 17:47:52 -0300

    entendendo o contexto de this na function e arrow function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,9 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        //o escopo lexico da arrow function faz com que funcione sem a Reflection.apply()
+        this._listaNegociacoes = new ListaNegociacoes( model => 
+            this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -20,7 +22,6 @@ class NegociacaoController {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = "Negociação adicionada com sucesso";
         this._mensagemView.update(this._mensagem);
@@ -32,7 +33,6 @@ class NegociacaoController {
     apaga() {
 
         this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,17 @@
 // criar modelo de lista de negociacoes para encapsular a lista no model
 class ListaNegociacoes {
 
-    constructor() {
+    constructor(armadilha) {
 
         this._negociacoes = [];
+        this._armadilha = armadilha;
     }
 
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
+        //Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
     get negociacoes() {
@@ -20,5 +23,7 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = [];
+        this._armadilha(this);
+        //Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 }
