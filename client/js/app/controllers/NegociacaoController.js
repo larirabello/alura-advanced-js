02701694017bba2d4fc132ac12ed5b0bc53a617c
Larissa Rabello <lari.rabello@gmail.com>
2017-08-20 17:34:19 -0300

    fazendo ajax com javascript vanilla

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -32,6 +32,37 @@ class NegociacaoController {
 
     }
 
+    importaNegociacoes() {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+        
+            /* ESTADOS:
+            0: Req. nao iniciada
+            1: conexao c servidor estabelecida
+            2: req. recebida
+            3: processando req.
+            4: req. concluida e a resposta está pronta
+            */
+            if(xhr.readyState == 4) {
+
+                if(xhr.status == 200) {
+                    
+                    JSON.parse(xhr.responseText)
+                    .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                    .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociacoes importadas com sucesso.'
+                } else {
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possivel obter as negociacoes';
+                }
+            }
+        };
+        xhr.send();
+    }
+
     apaga() {
 
         this._listaNegociacoes.esvazia();
